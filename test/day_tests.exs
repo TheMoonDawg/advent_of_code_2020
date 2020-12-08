023b7e47daa27f4e24c6172cff6cddcbd9a014e8
Eric Moon <emoon@aspirehealthcare.com>
2020-12-08 01:08:11 -0600

    Complete Day 5!

--- test/day_tests.exs
+++ test/day_tests.exs
@@ -5,7 +5,7 @@ defmodule DayTests do
   doctest Day2, import: true, tags: :day_2
   doctest Day3, import: true, tags: :day_3
   doctest Day4, import: true, tags: :day_4
-  # doctest Day5, import: true, tags: :day_5
+  doctest Day5, import: true, tags: :day_5
   # doctest Day6, import: true, tags: :day_6
   doctest Day7, import: true, tags: :day_7
 end
